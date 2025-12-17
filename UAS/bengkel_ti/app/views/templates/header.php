323638818928dba84e9cc8348fad3ea41afd024b
m.setyaadjie <135620776+setya20040304@users.noreply.github.com>
2025-12-17 20:02:45 +0700

    Update header.php

--- UAS/bengkel_ti/app/views/templates/header.php
+++ UAS/bengkel_ti/app/views/templates/header.php
@@ -4,25 +4,17 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Bengkel TI Unida</title>
-    <link rel="stylesheet" href="<?= BASEURL; ?>/css/main.css" />
+    
+    <?= vite('main.js'); ?>
+    
   </head>
   <body>
   <?php 
-      // Pastikan session dimulai di awal skrip
       if (session_status() === PHP_SESSION_NONE) {
           session_start();
       }
-    ?>
-    <!-- Navbar Section Start -->
-    <?php 
-// Pastikan session dimulai di awal skrip
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-// Menentukan role pengguna
-$role = $_SESSION['role'] ?? 'guest';
-?>
+      $role = $_SESSION['role'] ?? 'guest';
+  ?>
 
 <nav class="navbar">
   <style>.navbar { background-color: black; }</style>
@@ -31,8 +23,11 @@ $role = $_SESSION['role'] ?? 'guest';
     .navbar {
       font-family: 'Poppins', sans-serif;
       font-weight: 600;
-    }</style>
+    }
+  </style>
+  
   <p class="logo">#BengkelLaptop<span style="color: #e07b39;">TI.</span></p>
+  
   <div class="container">
     <ul class="navigation">
       <?php if ($role === 'admin'): ?>
@@ -45,22 +40,15 @@ $role = $_SESSION['role'] ?? 'guest';
         <li><a href="<?= BASEURL; ?>/catatan">Catatan</a></li>
         <li><a href="<?= BASEURL; ?>/daftar_perbaikan">Daftar Perbaikan</a></li>
       <?php endif; ?>
-      <!-- Tampilkan link login hanya untuk guest -->
+      
       <?php if ($role === 'guest'): ?>
         <li><a href="<?= BASEURL; ?>/login">Login</a></li>
       <?php endif; ?>
-      <!-- Menu untuk semua pengguna termasuk guest -->
+      
       <li><a href="<?= BASEURL; ?>/rating">Testimoni</a></li>
       <li><a href="<?= BASEURL; ?>/prices">Katalog Harga</a></li>
       <li><a href="<?= BASEURL; ?>/about">Tentang Kami</a></li>
       <li><a href="<?= BASEURL; ?>/home">Home</a></li>
-
-
     </ul>
   </div>
 </nav>
-
-<!-- Navbar Section End -->
-
-  </body>
-</html>
